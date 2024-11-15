fa9e9714aeffe49da57e21584f43314231d2a52d
donghoangau <donghoang.au@gmail.com>
2024-11-16 03:04:16 +1100

    Separate addItem function from test spec file

--- saucedemo.spec.js
+++ saucedemo.spec.js
@@ -1,5 +1,6 @@
 const { test, expect, errors } = require('@playwright/test')
 const selector = require('./selectors')
+const addItems = require('./addItems')
 
 test.describe('Saucedemo Swag Labs - E2E Regression Tests', () => {
   
@@ -8,33 +9,21 @@ test.describe('Saucedemo Swag Labs - E2E Regression Tests', () => {
     await page.fill(selector.loginPage.usernameInput, 'standard_user')
     await page.fill(selector.loginPage.passwordInput, 'secret_sauce')
     await page.click(selector.loginPage.loginButton)
-  });
-
-  async function addMultipleItems(page, numberOfItems) {
-    const items_count = await page.$$('.inventory_item')
-
-    if(numberOfItems>items_count) {
-      throw new Error(`Requested ${numberOfItems}, but only ${items_count} `)
-    }
-
-    for (let i = 0; i < numberOfItems; i++) {
-      await page.click(`.inventory_item:nth-of-type(${i + 1}) button`)
-    }
-  }
+  })
 
   // Test Case 1: Verify login with valid credentials
   test('Login with valid credentials', async ({ page }) => {
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
     const productPageTitle = await page.title()
     expect(productPageTitle).toContain('Swag Labs')
-  });
+  })
 
   // Test Case 2: Verify adding products to the cart
   test('Add product to cart', async ({ page }) => {
     await page.click(selector.inventoryPage.addToCartButtons) // Add first product to the cart
     const cartCount = await page.textContent(selector.inventoryPage.cartBadge)
     expect(cartCount).toBe('1')
-  });
+  })
 
   // Test Case 3: Verify removing products from the cart
   test('Remove product from cart', async ({ page }) => {
@@ -47,10 +36,10 @@ test.describe('Saucedemo Swag Labs - E2E Regression Tests', () => {
 
   // Test Case 4: Verify adding multiple products to cart
   test('Add multiple items to cart', async ({ page }) => {
-    await addMultipleItems(page, 4)
+    await addItems.addMultipleItems(page, 4)
     const cartCount = await page.textContent(selector.inventoryPage.cartBadge)
     expect(cartCount).toBe('4')
-  });
+  })
 
   // Test Case 5: Verify checkout with valid information
   test('Checkout process', async ({ page }) => {
@@ -63,7 +52,7 @@ test.describe('Saucedemo Swag Labs - E2E Regression Tests', () => {
     await page.click(selector.checkout.finisshButton)
     const confirmationText = await page.textContent('.complete-header')
     expect(confirmationText).toBe('Thank you for your order!')
-  });
+  })
 
   // Test Case 6: Verify logout
   test('Logout', async ({ page }) => {
